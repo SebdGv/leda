3dcfd0b4f98dd2e18b32f5e46885af7a9c03226a
bes dgv <bes.dgv@gmail.com>
2024-05-17 18:40:01 +0200

    y

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/leda/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  base: "/leda/",
 });
