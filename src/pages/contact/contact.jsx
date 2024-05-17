6ef821f61259fbaeefe07d4929de01433e16e2ae
bes dgv <bes.dgv@gmail.com>
2024-05-17 15:48:45 +0200

    work in progress

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,9 +1,36 @@
-import ImageSlider from "../../components/imageSlider/ImageSlider";
+import "./contact.scss";
 
 export default function Contact() {
   return (
     <>
-      <ImageSlider />
+      <section className="contact">
+        <p className="formTitle">Vous avez un projet ? Discutons-en !</p>
+        <form action="#" method="post">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Guy Georges"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="g.g@test.ex"
+            required
+          />
+          <textarea
+            name="message"
+            id="message"
+            cols="22"
+            rows="5"
+            placeholder="J'adore votre travail, faites vous de la livraison à domicile..."
+            required
+          ></textarea>
+          <button className="sendBtn">Envoyer</button>
+        </form>
+      </section>{" "}
     </>
   );
 }
